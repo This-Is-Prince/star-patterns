6f9820ccdf7a408f6ec9432c3bd0ef3e66dd29b8
Prince <this.is.kumar.prince@gmail.com>
2022-02-12 11:55:44 +0530

    second star pattern completed in java

--- in_java/Main.java
+++ in_java/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Pattern_1 pattern_1 = new Pattern_1(5, 5, "*");
         pattern_1.print();
+
+        Pattern_2 pattern_2 = new Pattern_2(5, 5, "*");
+        pattern_2.print();
     }
 }
